a51a462891ef4a5800dbf1ee78f7f48a70311b03
lgad31vn <lgad31vn@gmail.com>
2021-08-09 22:58:49 -0400

    Prepared for deployment

---
+++ client/src/reducers/deleteme.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const deleteme = props => {
+  return <div></div>;
+};
+
+deleteme.propTypes = {};
+
+export default deleteme;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
 const connectDB = require('./config/db');
 const path = require('path');
 
@@ -9,6 +8,8 @@ connectDB();
 
 //init Middleware
 app.use(express.json({ extended: false }));
+
+// app.get('/', (req, res) => res.send('API Running'));
 
 //define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -25,6 +26,8 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
+const PORT = process.env.PORT || 5000;
 
 // app.get('/', (req, res) => res.send('API running'));
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
